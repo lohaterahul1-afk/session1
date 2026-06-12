b1353f43cbb6fee2ad54a45af01eef18e2e95900
Rahul Lohate <lohaterahul1@gmail.com>
2026-06-12 21:54:29 +0530

    Add central bank application form in Bank.py

---
+++ Bank.py
@@ -0,0 +1,20 @@
+print("central bank of indai APPLICATION FORM 'SSOSS")
+
+account_no=9823979364
+name="Rahul"
+mobile="9823979364"
+email="lohaterahul1@gmail.com"
+address="Mandve"
+account_type="Saving"
+balance=10000000.00
+
+print("\n==========ACCOUNT DETAILS=========")
+print("Account Number:",account_no)
+print("Costomer Name: ",name)
+print("Mobile Number:",mobile)
+print("Email ID :",email)
+print("Adreess :",address)
+print("Account Type :",account_type)
+print("Balance :",balance)
+
+print("\nAccount Created Successfuly!")
